d7f69230b79337f6c9fe4fa77ce6a4a4e8036f51
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-12-26 09:40:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,21 +14,21 @@
 // }
 
 
-const list = document.querySelector('.output ul');
-list.innerHTML = '';
-const stations = ['MAN675847583748sjt567654;Manchester Piccadilly',
-                  'GNF576746573fhdg4737dh4;Greenfield',
-                  'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
-                  'SYB4f65hf75f736463;Stalybridge',
-                  'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'];
+// const list = document.querySelector('.output ul');
+// list.innerHTML = '';
+// const stations = ['MAN675847583748sjt567654;Manchester Piccadilly',
+//                   'GNF576746573fhdg4737dh4;Greenfield',
+//                   'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
+//                   'SYB4f65hf75f736463;Stalybridge',
+//                   'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'];
 
-for (const station of stations) {
-  // write your code just below here
-  const extractLetters = station.slice(0,3)
-  const findSemiColon = station.indexOf(';');
-  const extractName = station.slice(findSemiColon + 1)
-  const result = `${extractLetters}: ${extractName}`;
-  const listItem = document.createElement('li');
-  listItem.textContent = result;
-  list.appendChild(listItem);
-}
+// for (const station of stations) {
+//   // write your code just below here
+//   const extractLetters = station.slice(0,3)
+//   const findSemiColon = station.indexOf(';');
+//   const extractName = station.slice(findSemiColon + 1)
+//   const result = `${extractLetters}: ${extractName}`;
+//   const listItem = document.createElement('li');
+//   listItem.textContent = result;
+//   list.appendChild(listItem);
+// }
